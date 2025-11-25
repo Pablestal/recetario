47724768f30424d642b3546ad0b14e6173ca27e3
Pablo Estévez Álvarez <pablestal@gmail.com>
2025-11-25 18:36:35 +0100

    [RCT-33] User is allowed to modify recipies

--- src/pages/RecipeCreationPage.jsx
+++ src/pages/RecipeCreationPage.jsx
@@ -1,10 +1,13 @@
 import CreateRecipe from "../components/recipeForm/CreateRecipe";
+import { useParams } from "react-router-dom";
 
 const RecipeCreationPage = () => {
+  const { id } = useParams();
+
   return (
     <>
       <section>
-        <CreateRecipe />
+        <CreateRecipe recipeId={id} />
       </section>
     </>
   );
